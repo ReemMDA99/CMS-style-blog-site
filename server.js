f0b2c0a8e286827da46b9074c2b902e9543333c7
Reem Ahmed <reem.mda0909@gmail.com>
2022-05-26 09:27:05 -0600

    posts not shwoing on dashboard

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
   cookie: {expires: 10 * 60 * 1000},
-  resave: true,
+  resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
